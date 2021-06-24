7ab5344da3f01c76c5a2e2c97b515156f0bc63c3
andre <78390294+ankrudov@users.noreply.github.com>
2021-06-24 17:45:52 -0500

    added the comments and render campsite info components

--- src/components/CampsiteInforComponent.js
+++ src/components/CampsiteInforComponent.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
 
 const CampsiteInfo = (props) => {
-    renderCamspite(campsite){
+    const renderCampsite=(campsite)=>{
         return(
             <div className="col-md-5 m-1">
                 <Card>
@@ -13,19 +13,35 @@ const CampsiteInfo = (props) => {
                     </CardBody>
                 </Card>
             </div>
-        )
+    );
+    }
+    const renderComments=(comments)=>{
+        if (comments){
+            return(
+                <div className="col-md-5 m-1">
+                    <h4>Comments</h4>
+                    {comments.map(comment=>{
+                        return(
+                            <div key={comment.id}>
+                            <p>{comment.text}</p>
+                            <p>{comment.author}</p>
+                            <p>{new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</p>  
+                            </div>
+                        );
+                    })}
+                </div>
+            );
+        }return(<div/>);
     }
 
     if(props.campsite){ 
         return (
         <div className="row">
-            {renderCamspite(props.campsite)}
+            {renderCampsite(props.campsite)}
+            {renderComments(props.campsite.comments)}
         </div>  
-    )}else{
-        return(
-            <div/>
-        )
-    }   
-}
+    )}
+    return(<div/>);
+}   
 
 export default CampsiteInfo;
